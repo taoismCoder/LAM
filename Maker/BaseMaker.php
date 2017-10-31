40e6fbd5f3ad5444532ca946021396cecb0affab
vicleos <510331882@qq.com>
2017-10-31 09:59:52 -0500

    生成器基类必要方法

--- Maker/BaseMaker.php
+++ Maker/BaseMaker.php
@@ -6,10 +6,29 @@ namespace Taoism\LAM\Maker;
  * @package Taoism\LAM\Maker
  */
 interface BaseMaker {
-    /**
+    	/**
 	 * 获取模板文件路径
+	 * @return mixed
+	 */
+	public function getStub();
+	
+	/**
+	 * 设置模板文件路径
 	 * @param $path
 	 * @return mixed
 	 */
-	public function getStub($path);
+	public function setStub($path);
+	
+	/**
+	 * 获取原文
+	 * @return mixed
+	 */
+	public function getRawContents();
+	
+	/**
+	 * 设置原文
+	 * @param $raw
+	 * @return mixed
+	 */
+	public function setRawContents($raw);
 }
